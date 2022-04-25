612066b72a8acb4c27550ec11cf5cf132887e4da
Jason Cruz <jason1642@gmail.com>
2022-04-25 13:57:15 -0400

    443

--- src/components/HomePage/GroupChat/GroupChat.jsx
+++ src/components/HomePage/GroupChat/GroupChat.jsx
@@ -45,7 +45,7 @@ const Header = styled.div`
 
 const GroupChat = ({ currentUser }) => {
   const [socket, setSocket] = useState(null);
-  const url = window.location.hostname === 'localhost' ? 'ws://localhost:8080/' : 'https://circle-chat1.herokuapp.com'
+  const url = window.location.hostname === 'localhost' ? 'ws://localhost:8080/' : 'https://circle-chat1.herokuapp.com:443'
 
 
   useEffect(() => {
